c50bf6759c0a1c14965419f11d13a4f8791da670
yanzhang111 <yanz76@uci.edu>
2026-02-01 11:26:37 -0800

    3

--- MG4/Assets/Scripts/UIController.cs
+++ MG4/Assets/Scripts/UIController.cs
@@ -1,29 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
-    public Text scoreText;
+    public TMP_Text scoreText;
+    public GameObject gameOverPanel;
 
     void OnEnable()
     {
         GameController.OnScoreChanged += UpdateScore;
+        PlayerController.OnPlayerDied += ShowGameOver;
     }
 
     void OnDisable()
     {
         GameController.OnScoreChanged -= UpdateScore;
+        PlayerController.OnPlayerDied -= ShowGameOver;
     }
 
     void Start()
     {
         scoreText.text = "0";
+        gameOverPanel.SetActive(false);
     }
 
     private void UpdateScore(int newScore)
     {
         scoreText.text = newScore.ToString();
+    }
+
+    private void ShowGameOver()
+    {
+        gameOverPanel.SetActive(true);
     }
 }
